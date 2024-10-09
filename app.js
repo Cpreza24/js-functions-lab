61aebeb0e49e815bd5ecc3e04137ab3e4434dd44
Cesar Preza <cpreza93@yahoo.com>
2024-10-08 20:39:31 -0700

    adds exercise 7

--- app.js
+++ app.js
@@ -123,12 +123,35 @@ Complete the exercise in the space below:
 */
 
 function maxOfThree(num1, num2, num3) {
-
+    if (num1 > num2 && num1 > num3) {
+        return num1;
+    } else if (num2 > num1 && num2 > num3) {
+        return num2;
+    } else if(num3 > num1 && num3 > num2) {
+        return num3
+    }
 }
 
 console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
 
 
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+
+function calculateTip(billTotal, tipPercentage) {
+    return billTotal * (tipPercentage / 100); 
+}; 
+
+console.log('Exercise 7 Result:', calculateTip(50, 20));
 
 
 
