e7fbed6faad7c8eedce9f37393e49e07548b253e
Cesar Preza <cpreza93@yahoo.com>
2024-10-09 19:22:56 -0700

    finished functions lab

--- app.js
+++ app.js
@@ -154,4 +154,140 @@ function calculateTip(billTotal, tipPercentage) {
 console.log('Exercise 7 Result:', calculateTip(50, 20));
 
 
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+
+function convertTemperature(temp, scale) {
+    if (scale === 'C') {
+        return (temp * (9/5)) + 32;
+    } else if (scale === 'F') {
+        return ((temp - 32) * 5/9);
+    } else {
+        return 'Invalid Temperature Scale';
+    }
+};
+
+console.log('Exercise 8 Result:', convertTemperature(32, "C"));
+
+
+
+
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+
+function basicCalculator(num1, num2, operator) {
+    if (operator === 'addition') {
+        return num1 + num2;
+    } else if (operator === 'subtract') {
+        return num1 - num2;
+    } else if (operator === 'multiply') {
+        return num1 * num2;
+    } else if (operator === 'divide') {
+        return num1 / num2;;
+    };
+};
+
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
+
+/*
+Exercise 10: calculateGrade()
+
+Define a function called calculateGrade. 
+It should take a numerical score and return the corresponding letter 
+grade (A, B, C, D, F). 
+
+For example, 90 and above yields an 'A', 80-89 is a 'B', 
+and 70-79 is a 'C', 60-69 is a 'D' and anything lower than a 60 is an 'F'.
+
+Example: calculateGrade(100) should return A.
+
+Complete the exercise in the space below:
+*/
+
+function calculateGrade(studentScore) {
+    if (studentScore >= 90 && studentScore <= 100) {
+        return 'Your grade is an A';
+    } else if (studentScore >= 80 && studentScore < 90) {
+        return 'Your grade is an B';
+    } else if (studentScore >= 70 && studentScore < 80) {
+        return 'Your grade is an C';
+    } else if (studentScore >= 60 && studentScore < 70) {
+        return 'Your grade is an D';
+    } else {
+        return 'Your grade is an F';
+    }
+};
+
+console.log('Exercise 10 Result:', calculateGrade(80));
+
+
+/*
+Exercise 11: createUsername()
+
+Define a function called createUsername. 
+It should take a first name and a last name and return a username. 
+
+The username should be a combination of the following:
+- The first three letters of the first name.
+- The first three letters of the last name.
+- The total character count of the first and last name combined.
+
+Example: createUsername('Samantha', 'Green') should return 'SamGre13'.
+
+Complete the exercise in the space below:
+*/
+
+const createUsername = function(firstName, lastName) {
+    const newFirstName = firstName.slice(0, 3);
+    const newLastName = lastName.slice(0, 3);
+    const nameLength = firstName.length + lastName.length;
+    return `${newFirstName}${newLastName}${nameLength}`;
+};
+
+console.log('Exercise 11 Result:', createUsername("Samantha", "Green"));
+
+
+
+/*
+Exercise 12: numArgs()
+
+Challenge yourself with numArgs. 
+This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+
+function numArgs(arg1, arg2, arg3, arg4) {
+    // return the amount of arguments passed to the function
+    return arguments.length;
+}
+
+console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4));
+
+
+
 
